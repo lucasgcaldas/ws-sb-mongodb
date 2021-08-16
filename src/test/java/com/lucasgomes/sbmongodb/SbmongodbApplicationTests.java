2c77d225e8fed5a8f619f768146214b925d7a9c8
Lucas Gomes <lggomes007@gmail.com>
2021-08-16 11:02:45 -0300

    Projeto criado

---
+++ src/test/java/com/lucasgomes/sbmongodb/SbmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasgomes.sbmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
