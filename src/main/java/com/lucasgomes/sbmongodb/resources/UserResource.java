e866378ac3f28d5d1b624ce01d3b7f4205f318dc
Lucas Gomes <lggomes007@gmail.com>
2021-08-16 19:03:09 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/lucasgomes/sbmongodb/resources/UserResource.java
+++ src/main/java/com/lucasgomes/sbmongodb/resources/UserResource.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.lucasgomes.sbmongodb.domain.Post;
 import com.lucasgomes.sbmongodb.domain.User;
 import com.lucasgomes.sbmongodb.dto.UserDTO;
 import com.lucasgomes.sbmongodb.services.UserService;
@@ -48,7 +50,7 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
-	@RequestMapping(value ="/{id}", method = RequestMethod.DELETE)
+	@DeleteMapping(value ="/{id}")
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
@@ -60,5 +62,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value ="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
