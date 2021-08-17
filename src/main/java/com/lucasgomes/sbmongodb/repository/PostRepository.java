9e75fed0346d45b2a494d5a8f08129840ae4c7df
Lucas Gomes <lggomes007@gmail.com>
2021-08-16 21:25:45 -0300

    Consulta simples com @Query

--- src/main/java/com/lucasgomes/sbmongodb/repository/PostRepository.java
+++ src/main/java/com/lucasgomes/sbmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.lucasgomes.sbmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lucasgomes.sbmongodb.domain.Post;
@@ -10,5 +11,8 @@ import com.lucasgomes.sbmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regx: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
