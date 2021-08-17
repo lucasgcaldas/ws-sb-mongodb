f96e5357a48f04e66d8f18816be5f5fc130c0645
Lucas Gomes <lggomes007@gmail.com>
2021-08-16 21:59:52 -0300

    Consulta com varios criterios

--- src/main/java/com/lucasgomes/sbmongodb/resources/PostResource.java
+++ src/main/java/com/lucasgomes/sbmongodb/resources/PostResource.java
@@ -1,9 +1,11 @@
 package com.lucasgomes.sbmongodb.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,10 +29,22 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@RequestMapping(value ="/Titlesearch", method = RequestMethod.GET)
+	@GetMapping(value ="/titlesearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value ="/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
